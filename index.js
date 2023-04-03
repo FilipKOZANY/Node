64ae6e4ddf93e7e116b1e97c6b1969d4d3387e33
FilipKOZANY <filipkozany@seznam.cz>
2023-04-03 17:31:43 +0200

    02 - stranka

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -10,7 +11,7 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 const fs = require('fs')
-
+const score = fs.readFileSync('score.txt', 'utf-8');
 app.post('/score', (req, res) => {
   const { nickname } = req.body
   const scores = []
@@ -29,3 +30,46 @@ app.post('/score', (req, res) => {
   
   res.send(`Your score is ${average}`)
 })
+app.get('/scores', (req, res) => {
+  const fs = require('fs');
+  const score = fs.readFileSync('score.txt', 'utf-8');
+  res.render('scores', { score: score });
+});
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/submit', (req, res) => {
+  const prezdivka = req.body.prezdivka;
+  const score = Math.floor(Math.random() * 100) + 1;
+  const data = `${prezdivka}: ${score}\n`;
+  fs.appendFileSync('score.txt', data);
+  res.redirect('/scores');
+});
+
+app.listen(3000, () => {
+  console.log('Server běží na portu 3000');
+});
+app.get('/scores', (req, res) => {
+  const fs = require('fs');
+})
+app.post('/submit-score', (req, res) => {
+  // zpracování a zápis skóre do souboru
+  
+  res.redirect('/scores');
+});
+app.get('/scores', (req, res) => {
+  fs.readFile('scores.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Nastala chyba při čtení souboru se skóre.');
+      return;
+    }
+
+    const scores = JSON.parse(data);
+
+    res.render('scores', { scores: scores });
+  });
+});
--- leabord.js
+++ leabord.js
@@ -38,3 +38,30 @@ app.get('/leaderboard', (req, res) => {
 `)
 })
 })
+app.post('/submit-score', (req, res) => {
+  const { username, score } = req.body;
+  const data = `${username}: ${score}\n`;
+
+  fs.appendFile('scores.txt', data, (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Failed to submit score');
+      return;
+    }
+
+    res.send('Score submitted');
+  });
+});
+app.get('/scores', (req, res) => {
+  fs.readFile('scores.txt', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Failed to read scores');
+      return;
+    }
+
+    const scores = data.split('\n').filter(Boolean);
+
+    res.render('scores', { scores });
+  });
+});
